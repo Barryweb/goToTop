ffd241f2695fb8d45a217a020239019daada1fdb
Barryweb <40425206@gm.nfu.edu.tw>
2020-07-12 14:19:42 +0800

    finsh

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 window.onload = () =>{
-    console.log("完成");
+    var elements = document.getElementsByTagName("*")
+    //取得所有元素標籤
     
-    var test = document.getElementById("test");
-    
-    test.onclick = () =>{
-        var target = test.getAttribute("data-gte-target");
-        var offset = test.getAttribute("data-gte-offset");
-        console.log(target);
-        console.log(offset);
+    for (let index = 0; index < elements.length; index++) {
+        const e = elements[index];
+         e.onclick = () =>{
+        var target = e.getAttribute("data-gte-target");  //目標屬性
+        var offset = e.getAttribute("data-gte-offset"); //位移屬性
+            if (target) {
+                var element = document.getElementById(target);    //目標元素
+                //視窗捲動至({上方,行為})
+                window.scrollTo({
+                    top: element.offsetTop - offset,
+                    behavior:"smooth"
+                })
+            }
+        }
     }
 }
